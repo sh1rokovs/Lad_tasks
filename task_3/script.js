5515c50ed48f4df05e2fd2cb5479a8ef2478b9f2
sh1rokovs <hilallod@yandex.ru>
2022-08-12 18:49:56 +0300

    tsk3

--- task_3/script.js
+++ task_3/script.js
@@ -1,63 +1,68 @@
 const readlineSync = require('readline-sync');
 
-let countOfValue = +readlineSync.question('Enter count of number: ');
-let countOfTry = +readlineSync.question('Enter count of try(Infinity = 0): ')
-
-function checkWin(valueRobot, valueHuman, countOfValue) { //return countOfCow, countOfBulls
-    console.log(valueRobot, valueHuman, countOfValue)
+/**
+ * @name checkWin
+ * @description Функция проверяющая выигрыш.
+ * @param {string} valueRobot
+ * @param {string} valueHuman
+ * @param {number} countOfValue
+ * @returns {object} countOfCow, countOfBulls, valuesCow, valuesBulls
+ */
+function checkWin(valueRobot, valueHuman, countOfValue) {
     let valuesCow = []
     let valuesBulls = []
     let countOfCow = 0 // Цифры совпадают и на своих местах
     let countOfBulls = 0 // Такая цифра есть в числе, но не на своем месте
     for(let i = 0; i < countOfValue; i++) {
-        if(valueRobot.toString()[i] === valueHuman.toString()[i]) {
+        if(valueRobot[i] === valueHuman[i]) {
             countOfCow++
-            valuesCow.push(valueHuman.toString()[i])
+            valuesCow.push(valueHuman[i])
         }
         else {
-            let valueRobot1 = valueRobot.toString()
+            let valueRobot1 = valueRobot
             valueRobot1[i] = "*"
-            if(valueRobot1.includes(valueHuman.toString()[i]))
+            if(valueRobot1.includes(valueHuman[i]))
             {
                 countOfBulls++
-                valuesBulls.push(valueHuman.toString()[i])
+                valuesBulls.push(valueHuman[i])
             }
         }
     }
-    console.log(countOfCow, countOfBulls, valuesCow, valuesBulls)
     return [countOfCow, countOfBulls, valuesCow, valuesBulls]
 }
 
-function getValueRobot(countOfValue) { //return valueRobot
+/**
+ * @name getValueRobot
+ * @description Получение числа робота.
+ * @param {number} countOfValue
+ * @returns {string} valueRobot
+ */
+function getValueRobot(countOfValue) {
     let valueRobot = ""
     for(let i = 0; i < countOfValue; i++) {
-        valueRobot += Math.floor((Math.random() * 9) + 1).toString()
+        valueRobot += Math.floor((Math.random() * 9) + 1)
     }
-    console.log(valueRobot)
     return valueRobot
 }
 
-if(countOfTry < 0) {
-    countOfTry = 0
-}
-else if(countOfTry === 0){
-    countOfTry = true
-}
+let countOfValue = +readlineSync.question('Enter count of number: '); // Количество цифр в числе
+let countOfTry = +readlineSync.question('Enter count of try(Infinity = 0): ') // Количество попыток
+let valueRobot = getValueRobot(countOfValue) // Получение числа робота
 
-let valueRobot = getValueRobot(countOfValue)
+if (countOfTry < 0 || countOfTry === 0) countOfTry = true;
+
 while(countOfTry) {
-    if(countOfTry !== true){
+    if(countOfTry !== true){ // Если количество попыток задано, то уменьшаем.
         countOfTry--
     }
-    let valueHuman = +readlineSync.question('Enter number: ')
-    console.log(checkWin(valueRobot, valueHuman, countOfValue))
+    let valueHuman = readlineSync.question('Enter number: ') // Вводим свое число
     let [countOfCow, countOfBulls, valuesCow, valuesBulls] = checkWin(valueRobot, valueHuman, countOfValue)
 
-    if(countOfCow === countOfValue){
+    if(countOfCow === countOfValue){ // Проверяем выигрыш
         console.log(`You win, number is - ${valueRobot}`)
         countOfTry = false
     }
-    else {
-        console.log(`Matching numbers are out of place - ${countOfBulls}[${valuesBulls}], Numbers in place - ${countOfCow}[${valuesCow}]`)
+    else { // Иначе показываем количество быков и коров
+        console.log(`Matching numbers are out of place - ${countOfBulls}(${valuesBulls}), Numbers in place - ${countOfCow}(${valuesCow})`)
     }
 }

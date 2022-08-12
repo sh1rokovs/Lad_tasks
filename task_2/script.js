9ccda0a4b358e392a260af9701e7e8ef3abc1354
sh1rokovs <hilallod@yandex.ru>
2022-08-12 17:34:33 +0300

    tsk2

--- task_2/script.js
+++ task_2/script.js
@@ -2,3 +2,4 @@ let formElement = document.getElementById("formElement");
 
 formElement.addEventListener("focus", event => event.target.classList.add('focused'), true);
 formElement.addEventListener("blur", event => event.target.classList.remove('focused'), true);
+// Делегирование событий, вешаем событие на родительском элементе
